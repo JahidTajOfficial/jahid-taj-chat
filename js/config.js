ad2d9c21662e17a9b554cbcbe8c10db8f00ade64
JahidTajOfficial <jahidtaj32@gmail.com>
2026-05-01 13:45:26 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,8 @@
-const CONFIG = {
-  TURSO_URL: "libsql://jahid-taj-chat-jahid.aws-ap-south-1.turso.io",
+var CONFIG = {
+  TURSO_URL: "https://jahid-taj-chat-jahid.aws-ap-south-1.turso.io",
   TURSO_TOKEN: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3Nzc2MTU3ODgsImlkIjoiMDE5ZGUyMjYtYWIwMS03NzlkLTgyYjEtNjA2M2JmODI4ZmE2IiwicmlkIjoiYTQ0OWNiYTMtYmEzZC00YmRhLTlkZjktYTNkODgxYzI0NWM0In0.rHQVy4ZheakTAVyaLMRq8CFFSUubrO_JL0gY4TcgWhWbcbHhXxllmAW-RvUmjFX-ocpOtKIx9jNHNjaSyApHCg",
-
   WORKER_URL: "https://jahid-taj-storage.jahidhasanofficial397.workers.dev",
   AUTH_SECRET: "jahidtaj2024secret",
-
   APP_NAME: "Jahid Taj Chat",
   VERSION: "1.0.0",
 };
